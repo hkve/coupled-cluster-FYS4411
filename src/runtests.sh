9ecd7d3ca09a6ef8a1adc04aff9b8f44f61e1ea6
hkve <haakon.kvernmoen@gmail.com>
2023-04-14 17:13:11 +0200

    Added shell script to run all tests

--- src/runtests.sh
+++ src/runtests.sh
@@ -4,6 +4,11 @@ files=`ls ./src/tests/*.py`
 
 for file in $files
 do
-	echo $file
+	file=${file#"./"}
+	file=${file%".py"}
+	file=$(echo "$file" | sed 's/\//\./g')
+
+	python3 -m unittest ${file}	
 done
+
 # py -m unittest src.tests.
