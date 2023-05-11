2e9ee22b4c2d55f02d98359ddccea3a1627e394d
hkve <haakon.kvernmoen@gmail.com>
2023-05-11 18:14:46 +0200

    More theory

--- src/analysis/pls.py
+++ src/analysis/pls.py
@@ -1,19 +1,19 @@
 from ..basis.HarmonicsOscillator import HarmonicsOscillator
-from ..CC.CCD import CCD
+from ..CC.CCD import CCD, RCCD
 from ..HF.HF import HF, RHF
 import numpy as np
 
 def main():
-    ho = HarmonicsOscillator(L=72, N=2, spinrestricted=True, fast=True)
+    ho = HarmonicsOscillator(L=20, N=2, spinrestricted=True, fast=True)
     ho.calculate_OB()
     ho.calculate_TB()
 
     hf = RHF(ho)
     hf.run()
-    ho = hf.perform_basis_change(ho)
-    ho.restricted_to_unrestricted()
+    # ho = hf.perform_basis_change(ho)
+    # ho.restricted_to_unrestricted()
     print(ho.evaluate_energy())
-    ccd = CCD(ho)
+    ccd = RCCD(ho)
     ccd.run(tol=1e-8, p=0.3)
 
     print(ccd)
