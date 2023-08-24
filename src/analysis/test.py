6c5cbb03fd0392924bbaf9063ae373ff6cf8f5f2
hkve <haakon.kvernmoen@gmail.com>
2023-08-24 10:45:40 +0200

    Added empty CCSD script and a sandbox analysis script

---
+++ src/analysis/test.py
@@ -0,0 +1,43 @@
+from ..basis.HarmonicsOscillator import HarmonicsOscillator
+from ..HF.HF import HF, RHF
+from ..CC.CCD import CCD
+from ..CC.fastCCD import fastCCD
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pathlib as pl
+import pandas as pd
+import time
+
+def end(start):
+    elapsed = time.time_ns() - start
+    return elapsed *1e-6 # ms
+
+omega = 1.0
+ho = HarmonicsOscillator(N=6, R=7, omega=omega, spinrestricted=True)
+ho.calculate_OB()
+ho.load_TB("ho1.0.npz")
+
+
+ho.restricted_to_unrestricted()
+
+start = time.time_ns()
+hf = HF(ho).run()
+t = end(start)
+print(f"HF {t = } ms")
+
+ho = hf.perform_basis_change(ho)
+
+start = time.time_ns()
+ccd = CCD(ho).run()
+t = end(start)
+print(f"CCD {t = } ms")
+
+print(ccd.evaluate_energy())
+
+start = time.time_ns()
+ccd = fastCCD(ho).run()
+t = end(start)
+print(f"CCD {t = } ms")
+
+print(ccd.evaluate_energy())
