615e55a816460b208ba4d3eb5c21581857a46114
hkve <haakon.kvernmoen@gmail.com>
2023-09-12 18:31:07 +0200

    Started making two Lipkin subsystems class

--- src/basis/Lipkin.py
+++ src/basis/Lipkin.py
@@ -7,25 +7,27 @@ class Lipkin(Basis):
         self.eps = eps
         self.V = V
 
-        self.setup()
-
     def setup(self):
+        L, N = self.L_, self.N_
+        self.h_ = self.fill_H0(L, N)
+
+        self.v_ = self.fill_H1(L, N)
+        self.is_AS_ = np.allclose(self.v_, -self.v_.transpose(0,1,3,2))
+
+
+    def fill_H0(self, L, N):
         H0_lower = np.ones(self.N_)*-self.eps/2
         H0_upper = np.ones(self.N_)*self.eps/2
 
-        self.h_ = np.diag(np.r_[H0_lower, H0_upper])
+        return np.diag(np.r_[H0_lower, H0_upper])
+    
 
-        self.fill_H1()
-
-    def fill_H1(self):
-        self.v_ = self.v_ - self.v_.transpose(0,1,3,2)
-        L, N = self.L_, self.N_
-
+    def fill_H1(self, L, N):
         spin_equal = lambda i, j: i // N == j // N
         pos_equal = lambda i, j: i - N == j or i == j - N
 
-        v = self.v_
-
+        v = np.zeros_like(self.v_)
+        print(v.shape)
         # After some math
         for g in range(L):
             for d in range(L):
@@ -42,22 +44,31 @@ class Lipkin(Basis):
 
                         v[g, d, a, b] = self.V*dspin*(pos_direct - pos_exchange)
 
-        self.is_AS_ = np.allclose(v, -v.transpose(0,1,3,2))
+        return v
+    
 
-        # First test        
-        # for i in range(N):
-        #     for j in range(N):
-        #         i_bar = i + N
-        #         j_bar = j + N
+class TwoLipkins(Lipkin):
+    def __init__(self, N_each, eps, V, **kwargs):
+        N = 2*N_each
+        super().__init__(N=N, eps=eps, V=V, **kwargs)
+        self.eps = eps
+        self.V = V
 
-        #         self.v_[i, j, i_bar, j_bar] = self.V/2
-        #         self.v_[j, i, j_bar, i_bar] = self.V/2
+        self.setup()
 
-        #         self.v_[i, j, j_bar, i_bar] = -self.V/2
-        #         self.v_[j, i, i_bar, j_bar] = -self.V/2
+    def setup(self):
+        Lsub, Nsub = self.L_//2, self.N_//2
+        L = self.L_
+        
+        h = self.fill_H0(Lsub, Nsub)
+        v = self.fill_H1(Lsub, Nsub)
 
-        #         self.v_[i_bar, j_bar, i, j] = self.V/2
-        #         self.v_[j_bar, i_bar, j, i] = self.V/2
+    
+        I = np.eye(2)
+        self.h_ = np.kron(I, h)
 
-        #         self.v_[j_bar, i_bar, i, j] = -self.V/2
-        #         self.v_[i_bar, j_bar, j, i] = -self.V/2
+        self.v_ = np.zeros(shape=(L,L,L,L))
+        # Fill this to be like
+        self.v_ = np.kron(v, I)
+
+        print(self.v_.shape)
