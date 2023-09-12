5dfbe4e46547019e84ac16682285fa27ddc73c08
hkve <haakon.kvernmoen@gmail.com>
2023-09-12 16:46:30 +0200

    Stupid stupid naming

--- src/basis/Molecule.py
+++ src/basis/Molecule.py
@@ -10,9 +10,10 @@ class Molecule(Basis):
         super().__init__(L=2*mol.nao, N=mol.nelectron, spinrestricted=True)
 
         L = self.L_
+        self.energy_shift_ = mol.energy_nuc()
         self.s_ = mol.intor_symmetric("int1e_ovlp")
         self.h_ = mol.intor_symmetric("int1e_kin") + mol.intor_symmetric("int1e_nuc")
-        self.u_ = mol.intor("int2e").reshape(L, L, L, L).transpose(0, 2, 1, 3)
+        self.v_ = mol.intor("int2e").reshape(L, L, L, L).transpose(0, 2, 1, 3)
 
         if not spinrestricted:
             self.restricted_to_unrestricted()
